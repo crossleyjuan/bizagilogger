c46230fd944ba12cb6bae623fc6bd53ffd7cb04f
Cross <cross@djondb.com>
2017-06-14 12:52:46 +0200

    Initial commit

---
+++ BizagiLogger/Logger.cs
@@ -0,0 +1,77 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BizagiLogger
+{
+    public class Logger
+    {
+        private log4net.ILog _log;
+
+        private static Dictionary<string, Logger> _loggers = new Dictionary<string, Logger>();
+        private static object _lock = new object();
+
+        static Logger()
+        {
+            XmlConfigurator.Configure();
+        }
+
+        public static Logger getLogger(string name)
+        {
+            if (!_loggers.ContainsKey(name))
+            {
+                lock (_lock)
+                {
+                    if (!_loggers.ContainsKey(name))
+                    {
+                        ILog log4 = LogManager.GetLogger(name);
+                        Logger logger = new Logger(log4);
+                        _loggers.Add(name, logger);
+                    }
+                }
+            }
+            return _loggers[name];
+        }
+
+        private Logger(log4net.ILog log)
+        {
+            this._log = log;
+        }
+
+        public void Debug(string caseId, string message)
+        {
+            using (log4net.ThreadContext.Stacks["NDC"].Push(caseId))
+            {
+                this._log.Debug(message);
+            }
+        }
+
+        public void Warn(string caseId, string message)
+        {
+            using (log4net.ThreadContext.Stacks["NDC"].Push(caseId))
+            {
+                this._log.Warn(message);
+            }
+        }
+
+        public void Info(string caseId, string message)
+        {
+            using (log4net.ThreadContext.Stacks["NDC"].Push(caseId))
+            {
+                this._log.Info(message);
+            }
+        }
+
+        public void Error(string caseId, string message)
+        {
+            using (log4net.ThreadContext.Stacks["NDC"].Push(caseId))
+            {
+                this._log.Error(message);
+            }
+        }
+    }
+}
---
+++ TestCommand/Program.cs
@@ -0,0 +1,27 @@
+﻿using BizagiLogger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger log = Logger.getLogger("test");
+
+            log.Debug("123", "test1");
+
+            Logger log2 = Logger.getLogger("test");
+
+            log.Debug("124", "test2");
+            log.Debug("123", "test3");
+            log.Debug("123", "test4");
+            log.Debug("124", "test5");
+            Console.Read();
+        }
+    }
+}
